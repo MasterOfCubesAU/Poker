136411f0a9c9017a0178fca33fa3fe025c6fe086
FawnCobra <mitchellfuchs@hotmail.com.au>
2022-12-05 23:21:52 +1100

    initial Commit player class and test

---
+++ src/entities/Player.py
@@ -0,0 +1,19 @@
+from .CardSet import CardSet
+import config
+
+class Player():
+    def __init__(self, name: str, hand: CardSet, balance:int) -> None:
+        self.name = name
+        self.hand = [].append(hand)
+        self.bet = 0
+        self.balance = balance
+
+    def addbet(self, bet:int):
+        if (self.bet + bet) < config.minbet:
+            raise ValueError(f' Valule cannot be greater than {config.minbet}')
+        if bet > self.balance:
+            raise ValueError(' Valule cannot be less than the Player balance')
+        else:
+            self.bet += bet
+            self.balance -= bet
+
---
+++ tests/test_Player.py
@@ -0,0 +1,29 @@
+from src.entities.Player import Player
+from src.entities.CardSet import CardSet
+from src.entities.Card import Card
+import pytest
+
+def test_bet():
+    cardSet = CardSet()
+
+    cards_to_add = [Card("THREE", "HEARTS"), Card("TWO", "SPADES"), Card("THREE", "HEARTS")]
+    cardSet.add(cards_to_add)
+    user = Player("Player0", cardSet, 50)
+
+    assert user.bet == 0
+    assert user.balance == 50
+
+    user.addbet(12)
+
+    assert user.bet == 12
+    assert user.balance == 38
+
+    cards_to_add = [Card("THREE", "HEARTS"), Card("TWO", "SPADES"), Card("THREE", "HEARTS")]
+    cardSet.add(cards_to_add)
+    user = Player("Player0", cardSet, 50)
+
+    with pytest.raises(ValueError):
+        user.addbet(3)
+
+    with pytest.raises(ValueError):
+        user.addbet(55)
