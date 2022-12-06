9f0b4c030c9aedf9aa32d809f912f18f0061a422
MasterOfCubesAU <masterofcubesau@gmail.com>
2022-12-06 14:07:33 +1100

    Added example code to main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from src.entities.Card import Card
 from src.entities.CardSet import CardSet
 
+
 def main():
-    pass
-    # hand = CardSet()
-    # hand.add([Card("ACE", "SPADES")])
-    # print(Card("ACE", "SPADES") in hand)
-    # hand.draw()
-    # print(Card("ACE", "SPADES") in hand)
+    hand = CardSet()
+    hand.add([Card("ACE", "SPADES")])
+    print(Card("ACE", "SPADES") in hand)
+    hand.draw()
+    print(Card("ACE", "SPADES") in hand)
+
 
 if __name__ == "__main__":
     main()
