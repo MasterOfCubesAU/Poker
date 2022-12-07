a8ae1322837acbb0285730f03d67842de739cc3b
MasterOfCubesAU <38149391+MasterOfCubesAU@users.noreply.github.com>
2022-12-07 19:31:33 +1100

    Create install.py

---
+++ install.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+import shutil
+import sys
+
+VENV_PATH = ".POKER"
+PYTHON_BIN = sys.executable
+
+def getBinPath():
+    if os.name == "posix":
+        return fr"{VENV_PATH}/bin"
+    else:
+        return f"{VENV_PATH}\Scripts"
+
+
+def main():
+    if args.reinstall or args.clean:
+        shutil.rmtree(VENV_PATH)
+        print(f"Removed {VENV_PATH}")
+        if args.clean:
+            return
+    if not os.path.exists(VENV_PATH):
+        if os.name == "posix":
+            os.system(f"sudo apt install {PYTHON_BIN}-venv")
+        os.system(f"{PYTHON_BIN} -m venv {VENV_PATH}")
+        print(f"Created {VENV_PATH}")
+    print(f"Installing dependencies")
+    os.system(f"{os.path.join(getBinPath(), 'pip')} install -r requirements.txt")
+
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Setup script.')
+    parser.add_argument('--reinstall', action='store_true', help='Force clean install.')
+    parser.add_argument('--clean', action='store_true', help='Removes all dependencies.')
+    args = parser.parse_args()
+    main()
+    print(f"Done")
